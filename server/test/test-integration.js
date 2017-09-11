20b3726c8ce1b65e9e3c1753573241ab50b86f6b
Evan Harris <echarris@smcm.edu>
2017-09-11 13:51:01 -0400

    Adds test suite and package json pathing for travis deployment.

---
+++ server/test/test-integration.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const chai = require('chai');
+
+const should = chai.should();
+
+describe('Test', function () {
+  it('should succeed', function () {
+    true.should.be.true;
+  });
+});
