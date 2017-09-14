8ed5264f17bcc418947a6af04199e309903adb80
Evan Harris <echarris@smcm.edu>
2017-09-14 17:30:46 -0400

    Modifies spacing and stands up test files

--- client/src/components/result-page.js
+++ client/src/components/result-page.js
@@ -2,14 +2,12 @@ import React from "react";
 import { connect } from "react-redux";
 import { restartApp } from '../actions/index';
 
-
 class Result extends React.Component {
 
   goToStart(e) {
     e.preventDefault();
     this.props.dispatch(restartApp());
   }
-
 
     render(){
       return (
--- client/src/components/test/login-page.test.js
+++ client/src/components/test/login-page.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 
-// brackets?
 import LoginPage from '../login-page';
 
 describe('<LoginPage /> ', () => {
