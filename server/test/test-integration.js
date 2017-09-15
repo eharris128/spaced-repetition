06d0e6d6003cf585f89f977ba2dc1a67c7cc5eed
Evan Harris <echarris@smcm.edu>
2017-09-15 13:56:48 -0400

    Removes failing references

--- server/test/test-integration.js
+++ server/test/test-integration.js
@@ -7,11 +7,6 @@ const keys = require('../config/keys');
 
 const should = chai.should();
 
-const {Users} = require('../models/users');
-const {Questions} = require('../models/questions');
-const {app, runServer, closeServer} = require('../server');
-mongoose.connect(keys.MONGO_URI);
-
 describe('Test', function () {
   it('should succeed', function () {
     true.should.be.true;
