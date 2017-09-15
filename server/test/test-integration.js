1e35e3a7ac0668be38e4647729ffe3270b0965c9
Evan Harris <echarris@smcm.edu>
2017-09-15 13:59:33 -0400

    Removes references to keys

--- server/test/test-integration.js
+++ server/test/test-integration.js
@@ -3,7 +3,6 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const faker = require('faker');
 const mongoose = require('mongoose');
-const keys = require('../config/keys');
 
 const should = chai.should();
 
