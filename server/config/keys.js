75dfb1d0b1e0a430adf7f83bb372ce6927aafc4e
Evan Harris <echarris@smcm.edu>
2017-10-02 20:09:00 -0400

    Testing process.env

--- server/config/keys.js
+++ server/config/keys.js
@@ -2,8 +2,10 @@
 
 if (process.env.NODE_ENV === "production") {
   // we are in production
+  console.log('Production: ', process.env.NODE_ENV)
   module.exports = require("./prod");
 } else {
   // we are in development
+    console.log('Development: ', process.env.NODE_ENV)
   module.exports = require("./secret");
 }
